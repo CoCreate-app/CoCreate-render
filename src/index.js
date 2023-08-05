55188aa101c454acf19b66f8ca39abcd6ce63d01
frankpagan <frank@cocreate.app>
2023-08-05 10:01:15 -0600

    Add copyright notice and commercial licensing information
    This commit adds a copyright notice and commercial licensing information to the source code. The copyright notice specifies the license under which the software is distributed, while the commercial licensing information provides details on obtaining a commercial license for using the software without the copyleft provisions of the AGPLv3.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+/********************************************************************************
+ * Copyright (C) 2023 CoCreate and Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ ********************************************************************************/
+
+// Commercial Licensing Information:
+// For commercial use of this software without the copyleft provisions of the AGPLv3,
+// you must obtain a commercial license from CoCreate LLC.
+// For details, visit <https://cocreate.app/licenses/> or contact us at sales@cocreate.app.
+
 /*globals CustomEvent*/
 import Actions from '@cocreate/actions';
 import Observer from '@cocreate/observer';
